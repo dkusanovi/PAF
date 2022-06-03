28ac908ded80a60a5b0f17d9bb6126a6266869f8
Dora Kusanovic <dkusanovi@pmfst.hr>
2022-06-03 09:49:05 +0200

    modified:   Vjezbe_11/__pycache__/planet.cpython-310.pyc 	modified:   Vjezbe_11/zadatak_1.py

--- Vjezbe/Vjezbe_11/zadatak_1.py
+++ Vjezbe/Vjezbe_11/zadatak_1.py
@@ -2,7 +2,7 @@ import planet as p
 import matplotlib.pyplot as plt
 import numpy as np
 
-sunce_zemlja = p.Planet(np.array([1.486*10**(11), 0]), np.array([0.1, 0.1]), np.array([0, 29783]), np.array([0, 0]), np.array([0, 0]), np.array([0, 0]), 5.9742*10**(24), 1.989*10**(30))
+sunce_zemlja = p.Planet(np.array([1.496*10**(11), 0]), np.array([0.1, 0.1]), np.array([0, 29783]), np.array([0, 0]), np.array([0, 0]), np.array([0, 0]), 5.9742*10**(24), 1.989*10**(30))
                 # r1, r2, v1, v2, a1, a2, m1, m2
 x1, y1, x2, y2 = sunce_zemlja.range(100)
 
